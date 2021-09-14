85c6b1e86f0739056388bc8f283c7597513a8026
Harsh <harshupadhayay906@gmail.com>
2021-09-15 01:27:04 +0530

    Created header for BST

---
+++ Advanced_Data_Strucutre/Binary_Search_Tree.h
@@ -0,0 +1,29 @@
+
+typedef short DataType;
+
+struct BST_Node {
+    DataType value;
+    struct BST_Node *left, *right;
+};
+
+typedef struct BST_Node bstNode_t;
+
+enum status{
+    FLASE, TRUE
+};
+
+typedef enum status status_t;
+
+void printBST(bstNode_t *root);
+
+void inorder(bstNode_t *root);
+
+void preOrder(bstNode_t *root);
+
+void inorder(bstNode_t *root);
+
+status_t insertBST (bstNode_t *root, bstNode_t *node);
+
+status_t searchBST (bstNode_t *root, bstNode_t *node);
+
+status_t deleteNodeBst (bstNode_t *root, bstNode_t *node);
