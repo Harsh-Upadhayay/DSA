3aa78ca82224850b6af4a315c77595fec0690231
Harsh-Upadhayay <harshupadhayay906@gmail.com>
2021-10-10 01:56:03 +0530

    heaps added

--- Advanced_Data_Strucutre/printTree.c
+++ Advanced_Data_Strucutre/printTree.c
@@ -1,41 +1,16 @@
-#include <stdio.h>
-#include "Display_Tree.h"
 
-int _print_t(avlNode_t *tree, int is_left, int offset, int depth, char s[20][255])
+int _print_t(btNode_t *tree, int is_left, int offset, int depth, char s[20][255])
 {
     char b[20];
     int width = 5;
 
     if (!tree) return 0;
 
-//    sprintf(b, "(%03d)", tree->value, tree->bFactor);
-    if(tree->bFactor >= 0)
-        sprintf(b, "%d,+%d", tree->value, tree->bFactor);
-    else
-        sprintf(b, "%d,%d", tree->value, tree->bFactor);
+    sprintf(b, "(%03d)", tree->value);
 
     int left  = _print_t(tree->left,  1, offset,                depth + 1, s);
     int right = _print_t(tree->right, 0, offset + left + width, depth + 1, s);
 
-#ifdef COMPACT
-    for (int i = 0; i < width; i++)
-        s[depth][offset + left + i] = b[i];
-
-    if (depth && is_left) {
-
-        for (int i = 0; i < width + right; i++)
-            s[depth - 1][offset + left + width/2 + i] = '-';
-
-        s[depth - 1][offset + left + width/2] = '.';
-
-    } else if (depth && !is_left) {
-
-        for (int i = 0; i < left + width; i++)
-            s[depth - 1][offset - width/2 + i] = '-';
-
-        s[depth - 1][offset + left + width/2] = '.';
-    }
-#else
     for (int i = 0; i < width; i++)
         s[2 * depth][offset + left + i] = b[i];
 
@@ -55,12 +30,12 @@ int _print_t(avlNode_t *tree, int is_left, int offset, int depth, char s[20][255
         s[2 * depth - 1][offset + left + width/2] = '+';
         s[2 * depth - 1][offset - width/2 - 1] = '+';
     }
-#endif
 
     return left + width + right;
 }
 
-void print_t(avlNode_t *tree)
+
+void print_t(btNode_t *tree)
 {
     char s[20][255];
     for (int i = 0; i < 20; i++)
@@ -71,3 +46,22 @@ void print_t(avlNode_t *tree)
     for (int i = 0; i < 20; i++)
         printf("%s\n", s[i]);
 }
+
+
+void printBST(btNode_t* root, short space)
+{
+    if (root == NULL)
+        return;
+  
+    space += SPACE_GAP_LEVEL;
+  
+    printBST(root->right, space);
+  
+    printf("\n");
+    for (int i = SPACE_GAP_LEVEL; i < space; i++)
+        printf(" ");
+    printf("%d\n", root->value);
+  
+    printBST(root->left, space);
+}
+
